d3d1639e2f6a220b3fc469d5418dc5244d84cc4a
dorovka <i.serafimovich@list.ru>
2023-08-24 19:15:11 +0300

    v6

--- proj/advertisements/app_advertisements/models.py
+++ proj/advertisements/app_advertisements/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+class Advertisement(models.Model):
+    
+    class Meta:
+        db_table = 'advertisements'
+    
+    title = models.CharField('название', max_length=120)
+    descriptions = models.TextField('описание')
+    price = models.DecimalField('цена', max_digits=9, decimal_places=2)
+    trades = models.BooleanField('Торг', help_text='Если хотим торговаться')
+    date_now = models.DateField('Создание дата', auto_now_add= True)
+    date_update = models.DateField("Обновление дата", auto_now= True) 
+    def __str__(self): 
+        return f'<Advertisement: Advertisement(id={self.id}, title={self.title}, price={self.price})>'
+    '''
+    def __str__(self): 
+        return f'<Advertisement: Advertisement(id=1, title= "Заголовок №1", price=100.00)>'
+    '''
+    pass
+
