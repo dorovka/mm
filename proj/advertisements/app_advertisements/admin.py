fef0291dd000102ca552302755bc52ab7dc583f8
dorovka <i.serafimovich@list.ru>
2023-08-27 18:21:24 +0300

    v7

--- proj/advertisements/app_advertisements/admin.py
+++ proj/advertisements/app_advertisements/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
+from .models import Advertisement
 
+class Advertisementadmin(admin.ModelAdmin):
+    list_display = ['title', 'descriptions', 'price', 'trades',  'date_update']
+    list_filter = ['date_now', 'date_update', 'trades']
+    fieldsets= (('Первый блок', {'fields' : ('title', 'descriptions')}),
+    ('Второй блок', {'classes' : ('collapse',), 'fields' : ('price', 'trades')}),
+    )
+    actions = ['make_true', 'make_false', 'created_date']
+    
+    @admin.action(description='Обновить торг на True')
+    def make_true(self, request, queryset):
+        queryset.update(trades = True)
+        pass
+
+    @admin.action(description='Обновить торг на False')
+    def make_false(self, request, queryset):
+        queryset.update(trades = False)
+        pass
+
+admin.site.register(Advertisement, Advertisementadmin)
 # Register your models here.
--- proj/advertisements/app_advertisements/models.py
+++ proj/advertisements/app_advertisements/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.utils.html import format_html
+from django.contrib import admin
 
 # Create your models here.
 class Advertisement(models.Model):
@@ -10,7 +12,7 @@ class Advertisement(models.Model):
     descriptions = models.TextField('описание')
     price = models.DecimalField('цена', max_digits=9, decimal_places=2)
     trades = models.BooleanField('Торг', help_text='Если хотим торговаться')
-    date_now = models.DateField('Создание дата', auto_now_add= True)
+    date_now = models.DateTimeField('Создание дата', auto_now_add= True)
     date_update = models.DateField("Обновление дата", auto_now= True) 
     def __str__(self): 
         return f'<Advertisement: Advertisement(id={self.id}, title={self.title}, price={self.price})>'
@@ -18,5 +20,26 @@ class Advertisement(models.Model):
     def __str__(self): 
         return f'<Advertisement: Advertisement(id=1, title= "Заголовок №1", price=100.00)>'
     '''
+
+    @admin.display(description='Дата создания')
+    def created_date(self):
+        from django.utils import timezone
+        if self.date_now.date() == timezone.now().date():
+            created_time = self.date_now.time().strftime('%H:%M:%S')
+            return format_html(
+                '<span style = "color:green; font-weight: bold;">Сегодня в {}</span>', created_time
+            )
+        return self.date_now.strftime('%d.%m.%Y в %H:%M:%S')
+    
+    @admin.display(description='Дата последнего обновления')
+    def updated_date(self):
+        from django.utils import timezone
+        if self.date_update.date() == timezone.now().date():
+            updated_time = self.date_update.time().strftime('%H:%M:%S')
+            return format_html(
+                '<span style="color:blue; font-weight: bold;">Сегодня в {}</span>', updated_time
+            )
+        return self.date_update.strftime('%d.%m.%Y в %H:%M:%S')
+
     pass
 
